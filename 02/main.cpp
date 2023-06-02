b24902e7bc0f049d060cdafc541d3c3fbab3f880
maliabramovitch <107685274+maliabramovitch@users.noreply.github.com>
2023-06-02 15:01:40 +0300

    end

--- 02/main.cpp
+++ 02/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(int argc, char **argv) {
     try {
         Terminal t(argc-1, argv+1);
-        return 0;
     }
     catch (exception& e) {
         cerr << e.what();
     }
+    return 0;
 }
